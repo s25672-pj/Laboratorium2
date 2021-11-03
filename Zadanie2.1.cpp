4f577c05d44c57e15bedff13ff80fb1637f83c0e
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-11-03 18:10:53 +0100

    Add files via upload

---
+++ Zadanie2.1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int liczba;
+    int x = 0;
+    int y = 0;
+    cout << "Podaj liczbe: ";
+    cin >> liczba;
+    for (int i = 1; i <= liczba; i++) {
+        x = x + i;
+        
+        y = y + x;
+    }
+    cout << "Suma szeregu dla liczby " << liczba << " wynosi " << y << endl;
+
+    return 0;
+}
