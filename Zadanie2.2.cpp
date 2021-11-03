61fc104dc276b3b2c114dc7a742f3fad37a830c2
s25672-pj <92215054+s25672-pj@users.noreply.github.com>
2021-11-03 18:11:26 +0100

    Add files via upload

---
+++ Zadanie2.2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Podaj pierwsza liczbe: ";
+    cin >> a;
+    cout << "Podaj druga liczbe: ";
+    cin >> b;
+    cout << "Podane wartosci to: " << a << " i " << b << endl;
+    cout << "\n";
+    cout << "Wiersz o dlugosci 'a'" << endl;
+    for (int i = 1; i <= a; i++)
+    {
+        cout << "*";
+    }
+    cout << "\n";
+    cout << "\n";
+    cout << "Kolumna o dlugosci 'b'" << endl;
+    for (int i = 1; i <= b; i++)
+    {
+        cout << "*" << endl;
+    }
+    cout << "\n";
+    cout << "Prostokat gwiazdek o wymiarach 'a' na 'b':" << endl;
+    for (int i = 0;i < a;i++)
+    {
+        for (int j = 0;j < b;j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    cout << "\n";
+    cout << "Obwod prostakatu o wymiarach 'a' na 'b': " << endl;
+    for (int i = 0; i < b; i++)
+    {
+        cout << "*";
+    }
+    cout << "\n";
+
+    for (int i = 0; i < a - 2; i++)
+    {
+        cout << "*";
+        for (int j = 0; j < b - 2; j++)
+        {
+            cout << " ";
+        }
+        cout << "*" << endl;
+    }
+
+    for (int i = 0; i < b; i++)
+    {
+        cout << "*";
+    }
+
+    return 0;
+}
